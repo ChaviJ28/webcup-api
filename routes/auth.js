d6892ab36f8f5c0664ccdfc9c99771d730bc9849
Chavi Surujbhali <surujbhalichavi@gmail.com>
2024-05-06 10:29:02 +0400

    init

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 let express = require("express"),
     router = express.Router(),
-    middleware = require("../middleware/auth");
+    mysql = require("mysql"),
+    bcrypt = require("bcrypt"),
+    connection = mysql.createConnection(require("../config/database"));
 
 const jwt = require('jsonwebtoken');
 
@@ -17,126 +19,103 @@ function generateAccessToken(user) {
 }
 
 router.post("/login", async (req, res) => {
-    try {
-        var db = require("../models/user")
-        var user = await db.findOne({
-            email: req.body.email
-        });
 
-        if (!user) {
-            return res.status(500).json({
-                success: false,
-                message: "User not found!"
-            });
+    var sql =
+        `SELECT * FROM user WHERE email='${req.body.email}';`;
+
+
+    connection.query(sql, async (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err });
         }
 
-        if (!user.validPassword(req.body.password)) {
-            return res.status(500).json({
-                success: false,
-                message: "Invalid Password!"
-            });
-        }
-        const jwt = generateAccessToken(user);
+        console.log(sql);
+        console.log(results);
 
-        return res.status(200).json({
-            success: true,
-            data: {
-                id: newUser.id,
-                email: newUser.email,
-                jwt
+        if (results.length > 0) {
+            if (req.body.password == results[0].password) {
+                res.status(200).json({ data: results[0] });
+            } else {
+                return res.status(500).json({ message: "Invalid Password" });
             }
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: err });
-
-    }
+        } else {
+            return res.status(500).json({ message: "Invalid Credentials" });
+        }
+    })
 });
 
 router.post("/register", async (req, res) => {
-    try {
-        var db = require("../models/user")
-        newUser = new db({
-            name: req.body.name,
-            email: req.body.email,
-            country: req.body.country,
-            phone_number: req.body.phone_number,
-            verfied: false
-        });
 
-        newUser.password = newUser.generateHash(req.body.password);
-        newUser.save();
+    var sql =
+        'INSERT INTO user(name, email, country, verified, password) VALUES ("' + req.body.name + '", "' + req.body.email + '", "", false, "' + req.body.password + '")';
 
-        return res.status(200).json({
-            success: true,
-            data: newUser
-        })
 
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            success: false,
-            error: err.message
-        });
-    }
-});
-
-router.post("/send-otp", middleware.authenticateToken, async (req, res) => {
-    try {
-        var db = require("../models/otp");
-        otp = new db({
-            user: req.user.id,
-        });
-        otp.save();
-
-        return res.status(200).json({
-            success: true,
-        })
-
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            success: false,
-            error: err.message
-        });
-    }
-});
-
-router.post("/verify-otp", middleware.authenticateToken, async (req, res) => {
-    try {
-        var db = require("../models/otp");
-        var userdb = require("../models/user");
-        otpRecord = await db.findOne({
-            otp: req.body.otp,
-            user: req.user.id
-        });
-
-        if (!otpRecord) {
-            return res.status(500).json({
-                success: false,
-                message: "Invalid OTP!"
-            });
+    connection.query(sql, async (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err });
         }
 
-        await userdb.updateOne({
-            _id: req.user.id
-        }, {
-            verfied: true
-        });
+        return res.status(200).json({ data: results.insertId });
 
-        return res.status(200).json({
-            success: true,
-            message: "User Verified"
-        })
-
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            success: false,
-            error: err.message
-        });
-    }
+    })
 });
+
+// router.post("/send-otp", async (req, res) => {
+//     try {
+//         var db = require("../models/otp");
+//         otp = new db({
+//             user: req.user.id,
+//         });
+//         otp.save();
+
+//         return res.status(200).json({
+//             success: true,
+//         })
+
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).json({
+//             success: false,
+//             error: err.message
+//         });
+//     }
+// });
+
+// router.post("/verify-otp", middleware.authenticateToken, async (req, res) => {
+//     try {
+//         var db = require("../models/otp");
+//         var userdb = require("../models/user");
+//         otpRecord = await db.findOne({
+//             otp: req.body.otp,
+//             user: req.user.id
+//         });
+
+//         if (!otpRecord) {
+//             return res.status(500).json({
+//                 success: false,
+//                 message: "Invalid OTP!"
+//             });
+//         }
+
+//         await userdb.updateOne({
+//             _id: req.user.id
+//         }, {
+//             verfied: true
+//         });
+
+//         return res.status(200).json({
+//             success: true,
+//             message: "User Verified"
+//         })
+
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).json({
+//             success: false,
+//             error: err.message
+//         });
+//     }
+// });
 
 
 module.exports = router;
